97386ec101ba9ea1cf9ed70f76ae065fb9400028
Stefan Shklifov <s.shklifov@gmail.com>
2023-08-13 14:17:22 +0300

    Rewrite main stdin loop

--- lsp/Main.cpp
+++ lsp/Main.cpp
@@ -16,109 +16,71 @@
 
 template <typename T>
 void writeResponse(int id, const T& resp) {
-    rapidjson::StringBuffer output;
-    rapidjson::Writer<rapidjson::StringBuffer> handle(output);
-    BufferWriter w(handle);
+	rapidjson::StringBuffer output;
+	rapidjson::Writer<rapidjson::StringBuffer> handle(output);
+	BufferWriter w(handle);
 
-    BufferWriter::ObjectScope root = w.beginObject();
-    w.add("jsonrpc", "2.0");
-    w.add("id", id);
-    w.add("result", resp);
+	BufferWriter::ObjectScope root = w.beginObject();
+	w.add("jsonrpc", "2.0");
+	w.add("id", id);
+	w.add("result", resp);
 
-    int bufferSize = output.GetSize();
-    printf("Content-Length: %d\r\n\r\n", bufferSize);
-    fputs(output.GetString(), stdout);
-    fflush(stdout);
+	int bufferSize = output.GetSize();
+	printf("Content-Length: %d\r\n\r\n", bufferSize);
+	fputs(output.GetString(), stdout);
+	fflush(stdout);
 }
 
 template <typename T>
 void writeNotification(const char* method, const T& resp) {
-    rapidjson::StringBuffer output;
-    rapidjson::Writer<rapidjson::StringBuffer> handle(output);
-    BufferWriter w(handle);
+	rapidjson::StringBuffer output;
+	rapidjson::Writer<rapidjson::StringBuffer> handle(output);
+	BufferWriter w(handle);
 
-    BufferWriter::ObjectScope root = w.beginObject();
-    w.add("jsonrpc", "2.0");
-    w.add("method", method);
-    w.add("params", resp);
+	BufferWriter::ObjectScope root = w.beginObject();
+	w.add("jsonrpc", "2.0");
+	w.add("method", method);
+	w.add("params", resp);
 
-    int bufferSize = output.GetSize();
-    printf("Content-Length: %d\r\n\r\n", bufferSize);
-    fputs(output.GetString(), stdout);
-    fflush(stdout);
+	int bufferSize = output.GetSize();
+	printf("Content-Length: %d\r\n\r\n", bufferSize);
+	fputs(output.GetString(), stdout);
+	fflush(stdout);
 }
 
-void stdinFunction() {
-	std::string str;
-	const std::string_view kContentLength("Content-Length: ");
-	bool received_exit = false;
-	while (true) {
-		int len = 0;
-		str.clear();
-		while (true) {
-			int c = getchar();
-			if (c == EOF) {
-                goto quit;
-            } else if (c == '\n') {
-				if (str.empty()) {
-                    break;
-                }
-				if (!str.compare(0, kContentLength.size(), kContentLength)) {
-					len = atoi(str.c_str() + kContentLength.size());
-                }
-				str.clear();
-			} else if (c != '\r') {
-				str += c;
-			}
-		}
+void processJson(rapidjson::Document& document) {
+	if (!document.HasMember("jsonrpc") || strcmp(document["jsonrpc"].GetString(), "2.0") != 0) {
+		return;
+	}
+	if (!document.HasMember("method")) {
+		return;
+	}
 
-        if (len == 0) {
-            goto quit;
-        }
+	const char* method = document["method"].GetString();
+	if (strcmp(method, "initialize") == 0) {
+		// TODO check id
+		int id = document["id"].GetInt();
+		writeResponse(id, InitializeResult());
+	} else if (strcmp(method, "initialize") == 0) {
+		// Do Nothing
+	} else if (strcmp(method, "textDocument/didOpen") == 0) {
+		ValueReader reader(document.GetObject());
+		DidOpenTextDocumentParams params;
+		reader.read(params);
+	}
 
-		str.resize(len);
-		for (int i = 0; i < len; ++i) {
-			int c = getchar();
-			if (c == EOF) goto quit;
-			str[i] = c;
-		}
-
-		auto message = std::make_unique< char[] >(len);
-		std::copy(str.begin(), str.end(), message.get());
-
-		/* FILE* fp = fopen("/home/stef/viml-server/stdin.txt", "a"); */
-		/* assert(fp); */
-		/* fprintf(fp, "Content-Length: %d\n%s\n", len, message.get()); */
-		/* fclose(fp); */
-
-        rapidjson::Document document;
-		document.Parse(message.get(), len);
-		assert(!document.HasParseError());
-
-        const char* method = document["method"].GetString();
-        if (strcmp(method, "initialize") == 0) {
-            int id = document["id"].GetInt();
-            writeResponse(id, InitializeResult());
-        } else if (strcmp(method, "initialize") == 0) {
-            // Do Nothing
-        } else if (strcmp(method, "textDocument/didOpen") == 0) {
-            ValueReader reader(document.GetObject());
-            DidOpenTextDocumentParams params;
-            reader.read(params);
-        }
-
-        /* PublishDiagnosticsParams params; */
-        /* Diagnostic dig; */
-        /* dig.message = "Underfined reference to server"; */
-        /* dig.severity = Diagnostic::Error; */
-        /* dig.range.start.line = 1; */
-        /* dig.range.start.character = 1; */
-        /* dig.range.end.line = 1; */
-        /* dig.range.end.character = 2; */
-        /* params.diagnostics.push_back(dig); */
-        /* params.uri = "file:///home/stef/.vimrc"; */
-        /* writeNotification("textDocument/publishDiagnostics", params); */
-        /* return; */
+	/* PublishDiagnosticsParams params; */
+	/* Diagnostic dig; */
+	/* dig.message = "Underfined reference to server"; */
+	/* dig.severity = Diagnostic::Error; */
+	/* dig.range.start.line = 1; */
+	/* dig.range.start.character = 1; */
+	/* dig.range.end.line = 1; */
+	/* dig.range.end.character = 2; */
+	/* params.diagnostics.push_back(dig); */
+	/* params.uri = "file:///home/stef/.vimrc"; */
+	/* writeNotification("textDocument/publishDiagnostics", params); */
+	/* return; */
 
 #if 0
 
@@ -139,11 +101,60 @@ void stdinFunction() {
 
 		if (received_exit) break;
 #endif
+}
+
+void stdinFunction() {
+	const char* expectedMsg = "Content-Length: ";
+	int charsMatched = 0;
+	while (true) {
+		int c = getchar();
+		if (c == EOF) {
+			break;
+		} else if (c == '\r' || c == '\n') {
+			charsMatched = 0;
+		} else if (c == expectedMsg[charsMatched]) {
+			++charsMatched;
+		} else {
+			while (c != '\n' && c != EOF) {
+				c = getchar();
+			}
+			charsMatched = 0;
+		}
+
+		if (expectedMsg[charsMatched] == '\0') {
+			// Read message length
+			int len = 0;
+			int s = scanf("%d", &len);
+			if (s == 1) {
+				char* message = new char[len + 1];
+				// Skip to message
+				do {
+					c = getchar();
+				} while (c == '\n' || c == '\r');
+				message[0] = c;
+				// Read message
+				for (int i = 1; i < len; ++i) {
+					if (c == EOF) {
+						break;
+					}
+					c = getchar();
+					message[i] = c;
+				}
+				if (c != EOF) {
+					message[len] = '\0';
+					// Parse as json
+					rapidjson::Document document;
+					document.Parse(message, len);
+					assert(!document.HasParseError());
+					processJson(document);
+					delete[] message;
+				}
+			}
+		}
 	}
 
-quit:
-	if (!received_exit) {
 #if 0
+	if (!received_exit) {
 		const std::string_view str("{\"jsonrpc\":\"2.0\",\"method\":\"exit\"}");
 		auto message = std::make_unique< char[] >(str.size());
 		std::copy(str.begin(), str.end(), message.get());
@@ -154,22 +165,22 @@ quit:
 		assert(fp);
 		fprintf(fp, "FOUND QUIT: %s\n", message.get());
 		fclose(fp);
-#endif
 		return;
 	}
+#endif
 }
 
 int main() {
-    /* FILE* fp = fopen("/home/stef/viml-server/stdin.txt", "w"); */
-    /* fclose(fp); */
+	/* FILE* fp = fopen("/home/stef/viml-server/stdin.txt", "w"); */
+	/* fclose(fp); */
 
-    /* int c = fgetc(stdin); */
-    /* while (c != EOF) { */
-    /*     fp = fopen("/home/stef/viml-server/stdin.txt", "a"); */
-    /*     fputc(c, fp); */
-    /*     fclose(fp); */
-    /*     c = fgetc(stdin); */
-    /* } */
+	/* int c = fgetc(stdin); */
+	/* while (c != EOF) { */
+	/*	   fp = fopen("/home/stef/viml-server/stdin.txt", "a"); */
+	/*	   fputc(c, fp); */
+	/*	   fclose(fp); */
+	/*	   c = fgetc(stdin); */
+	/* } */
 	stdinFunction();
 	return 0;
 }
